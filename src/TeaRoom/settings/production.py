413546d8ddb7f97634b5432a596c19bb439628b3
Data Analysis <da@marder.physik.uzh.ch>
2015-09-03 11:36:31 +0200

    some mods for deployment

--- src/TeaRoom/settings/production.py
+++ src/TeaRoom/settings/production.py
@@ -1,1 +1,14 @@
 from .base import *
+
+# Destination of error alerts
+ADMINS = (
+    ("Elena", "elena.graverini@cern.ch"),
+    ("Barbara", "barbara.storaci@cern.ch"),
+    ("Marco", "mtresch@physik.uzh.ch"),
+)
+
+# Send a notification to the managers every time
+# somebody ends up onto a 404 with a non-empty
+# referrer (i.e. a broken link)
+SEND_BROKEN_LINK_EMAILS = True
+MANAGERS = ADMINS
