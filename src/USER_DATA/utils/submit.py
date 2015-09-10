045e79213769adc6b688cbe1f7ad15bc04f9e9dc
Data Analysis <da@marder.physik.uzh.ch>
2015-09-10 11:41:37 +0200

    For some reason, we cannot get cookies from the upload-assignment view. Quick fix.

--- src/USER_DATA/utils/submit.py
+++ src/USER_DATA/utils/submit.py
@@ -16,8 +16,8 @@ from subprocess import Popen, PIPE, STDOUT
 
 #URL = 'http://localhost:8000/upload-result/'
 # URL = 'https://%s/upload-result/' % ('SITE_URL')  # to change after the deploy
-URL = 'http://marder.physik.uzh.ch/da/upload-assignment/'
-COOKIE_URL = 'http://marder.physik.uzh.ch/da/'
+URL = 'https://marder.physik.uzh.ch/da/upload-assignment/'
+COOKIE_URL = 'https://marder.physik.uzh.ch/da/'
 
 # FILES_TO_COMPLETE = []
 # EXERCISE_ID = 1
@@ -69,10 +69,10 @@ def upload_package(auth_data, filename):
 
 def download_and_execute_test(auth_data):
     s = requests.session()
-    r = s.get(URL, verify=False)
+    #r = s.get(URL, verify=False)
 
     # get csrftoken from server
-    r = s.get(URL, verify=False)
+    r = s.get(COOKIE_URL, verify=False)
     csrftoken = r.cookies['csrftoken']
     headers = {'X-CSRFToken': csrftoken, 'Referer': URL}
 
